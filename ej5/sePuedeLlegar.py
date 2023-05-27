d8d428e13e4a9839e127c57a02ed3b1aa55a1939
mscrimaglia <maxiscrimaglia@gmail.com>
2023-05-27 16:57:18 -0300

    ej5 90%

---
+++ ej5/sePuedeLlegar.py
@@ -0,0 +1,38 @@
+from typing import List
+from typing import Tuple
+
+# Aclaración: Debido a la versión de Python del CMS, para el tipo Lista y Tupla, la sintaxis de la definición de tipos que deben usar es la siguiente:
+# l: List[int]  <--Este es un ejemplo para una lista de enteros.
+# t: Tuple[str,str]  <--Este es un ejemplo para una tupla de strings.
+# Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
+def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> int :
+  cantidad_vuelos = 0
+  origen_actual = origen
+  while hayVueloDesdeOrigen(origen_actual, vuelos):
+    cantidad_vuelos += 1
+    if destinoDesdeOrigen(origen_actual, vuelos) == destino:
+      return cantidad_vuelos
+    else:
+      origen_actual = destinoDesdeOrigen(origen_actual, vuelos)
+  return -1
+
+    
+def hayVueloDesdeOrigen(origen: str, vuelos: List[Tuple[str, str]]) -> bool:
+  for vuelo in vuelos:
+    if vuelo[0] == origen:
+      return True
+  return False
+
+def destinoDesdeOrigen(origen: str, vuelos: List[Tuple[str, str]]) -> Tuple:
+  for vuelo in vuelos:
+    if vuelo[0] == origen:
+      return vuelo[1]
+
+if __name__ == '__main__':
+  """ origen = input()
+  destino = input()
+  vuelos = input()
+  
+  print(sePuedeLlegar(origen, destino, [tuple(vuelo.split(',')) for vuelo in vuelos.split()])) """
+
+  print(sePuedeLlegar('madrid','buenos aires', [('madrid', 'barcelona'), ('roma', 'berlin'), ('barcelona', 'roma')]))
